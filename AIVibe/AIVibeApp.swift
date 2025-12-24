99673a822746b064f08cf53c88816a8a51b6652b
md851600 <md851600@gmail.com>
2025-12-23 22:35:03 -0500

    Initial Commit

---
+++ AIVibe/AIVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIVibeApp.swift
+//  AIVibe
+//
+//  Created by Sarah Zhang on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AIVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
